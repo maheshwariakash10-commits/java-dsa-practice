809dcf8665a95bc06743c8e0545e96937c89c9f4
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-05-07 23:44:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
+        // Tc O(1)
         if(num==0) return true;
         if(num%10==0|| num<=0)return false;
         return true;
