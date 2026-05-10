94085b2a092db66ba8dc8c8279c240b360ec1ede
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-05-10 14:46:09 +0530

    Time: 1 ms (99.81%), Space: 42.8 MB (92.3%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char n[]= s.toCharArray();
+        char m[]= t.toCharArray();
+        int xor=0;
+
+        for(int i=0;i<n.length;i++){
+            xor=xor^(int)n[i];
+        }
+
+        for(int i=0;i<m.length;i++){
+            xor=xor^ m[i];
+        }
+
+        
+        return (char)(xor);
+    }
+}
