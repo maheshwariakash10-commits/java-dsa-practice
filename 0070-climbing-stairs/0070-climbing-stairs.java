4d0ddc1a9fcda1e71809179faffd5507e9687faf
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-05-11 14:04:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1||n==2) return n;
+        int n1=1;
+        int n2=2;
+        int nf=0;
+        for(int i=3;i<=n;i++){
+            nf=n1+n2;
+            n1=n2;
+            n2= nf;
+        }
+        return nf;
+    }
+}
