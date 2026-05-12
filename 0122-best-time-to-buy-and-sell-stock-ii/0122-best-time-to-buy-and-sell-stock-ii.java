c8c0da33a502eff8c6855bc3e52b5769c1321e5e
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-05-12 17:55:51 +0530

    Time: 0 ms (100%), Space: 45.7 MB (99.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,12 +2,14 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int netprofit=0;
+        // {}
         for(int i=0;i<n-1;i++){
-            int dff= prices[i+1]-prices[i];
-            if(dff>=0){
-                netprofit=netprofit+dff;
+            int dff= prices[i+1]-prices[i];//{7,1} 
+            if(dff>0){ // false
+                netprofit= netprofit + dff;
             }
         }
         return netprofit;
+
     }
 }
