db6d51ec2629376ca27d520dc780289ca641e347
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-05-24 15:54:33 +0530

    Time: 14 ms (48.76%), Space: 41.9 MB (70.63%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,16 +3,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low=0;
-        int high=n;
+        int low= 0;
+        int high =n;
+        int idx=0;
 
         while(low<=high){
             int mid= low+(high-low)/2;
 
-            if(isBadVersion(mid)==true) high=mid-1;
-            else if(isBadVersion(mid)==false) low=mid+1;   
+            if(isBadVersion(mid)==true) {
+                high= mid-1;
+                idx= mid;
+            }
+            else low=mid+1;
         }
-        return low;
-        
+
+        return idx;
     }
 }
