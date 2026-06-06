732519dbac8fee7697dc8076f071f0092df431a6
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-06-06 11:40:01 +0530

    Time: 0 ms (100%), Space: 44.2 MB (13.18%) - LeetHub

--- 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -1,15 +1,12 @@
 class Solution {
     public int returnToBoundaryCount(int[] nums) {
-        // tc= O(n), 
         int n= nums.length;
-        int bount=0;
+        int sum=0;
         int count=0;
-        for(int i=0;i<n;i++){
-            bount= bount+nums[i];
-            if(bount==0) count++;
+        for(int i=0 ;i<n ;i++){
+            sum= sum+nums[i];
+            if(sum==0) count++;
         }
         return count;
-
-        
     }
 }
