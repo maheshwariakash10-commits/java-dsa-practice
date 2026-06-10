ca4418de8c9efbffd689c6d996b3bb734ef9ed21
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-06-10 20:20:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (34.63%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,28 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
-        return happy(n);
-        
-    }
-
-    public boolean happy(int n){
         if(n<=9) {
-
             if(n==1||n==7) return true;
-
-            else{
-                return false;
-            }
+            return false;
         }
 
         int cp =n;
         int pdt=0;
-        
         while(cp!=0){
             int rem= cp%10;
             pdt= pdt+(rem*rem);
             cp/=10;
         }
-
-        return happy(pdt);
+        return isHappy(pdt);
+        
     }
 }
