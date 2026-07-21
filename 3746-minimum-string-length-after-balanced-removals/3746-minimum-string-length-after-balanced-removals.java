428decfb97c0e19cc83873ccdad971496c166fb2
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-07-21 22:50:16 +0530

    Time: 30 ms (19.73%), Space: 47.2 MB (39.65%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -1,12 +1,18 @@
 class Solution {
     public int minLengthAfterRemovals(String s) {
-        int countA=0;
-        int countB=0;
-        int n= s.length();
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='a') countA++;
-            if(s.charAt(i)=='b') countB++;
+        Stack<Character> stack= new Stack<>();
+        for(int i=0 ;i< s.length(); i++){
+            char ch= s.charAt(i);
+            if(stack.isEmpty()){
+                stack.push(ch);
+            }
+            else if(stack.peek()== ch){
+                stack.push(ch);
+            }
+            else{
+                stack.pop();
+            }
         }
-        return Math.abs(countA-countB);
+        return stack.size();
     }
 }
