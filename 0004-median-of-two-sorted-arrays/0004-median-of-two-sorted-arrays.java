a2e71c316748aa97a66fd0c32af1360d6d814134
Akash Maheshwari <maheshwariakash.10@gmail.com>
2026-08-16 10:14:42 +0530

    Time: 2 ms (47.14%), Space: 49 MB (15.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,21 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n= nums1.length;
-        int m= nums2.length;
+        int n2= nums2.length;
 
-        int arr[]= new int[n+m];
-        int i=0;
-        for(int j=0; j< n; j++){
-            arr[i]= nums1[j];
-            i++;
-        }
-        for(int j=0; j< m; j++){
-            arr[i]= nums2[j];
-            i++;
-        }
-
-        Arrays.sort(arr);
+        int arr[]= new int[n+n2];
         int l= arr.length;
+
+        int i=0; // for  num1
+        int j= 0; // for num2;
+
+        for(int k=0 ; k< l ; k++ ){
+            
+            if(i< n && j< n2 ){
+                int mn= Math.min(nums1[i], nums2[j]);
+                arr[k]= mn;
+                if(mn== nums1[i]) i++;
+                else j++;
+
+            }
+
+            else if( i< n){
+                arr[k] = nums1[i];
+                i++;
+            }
+            else{
+                arr[k] = nums2[j];
+                j++;
+            }
+        }
+
+
+
+
+
+        
 
         if(l%2==0){
             return (double)(arr[l/2] + arr[l/2 - 1])/2;
